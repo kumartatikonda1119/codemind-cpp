cfe6258336936baa95e2e62281c044ff837c4a54
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-03-07 21:25:36 +0530

    #include<iostream> using namespace std; int main() {     int a;     cin>>a;     int area,perimeter;     area=a*a;     perimeter=4*a;     cout<<area<<" "<<perimeter;
    }

---
+++ Area_and_Perimeter_of_Square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int area,perimeter;
+    area=a*a;
+    perimeter=4*a;
+    cout<<area<<" "<<perimeter;
+    
+    
+}
