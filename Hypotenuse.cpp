c57f7af72a59bc7d7c3923a22704825f2faa5180
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-03-10 11:37:16 +0530

    #include<iostream> #include<cmath> #include<iomanip> using namespace std; int main() {     int a,b;     cin>>a>>b;     float c=sqrt((a*a)+(b*b));     cout<<fixed<<setprecision(2)<<c;
    }

---
+++ Hypotenuse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    float c=sqrt((a*a)+(b*b));
+    cout<<fixed<<setprecision(2)<<c;
+    
+    
+}
