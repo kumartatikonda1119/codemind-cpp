99b0a2b360043c8d10bee794e4d60b7429475d52
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-03-13 19:55:02 +0530

    #include<iostream> using namespace std; int main() {     int a;     cin>>a;     int k=a*4;     if(k<=1000)     {         cout<<"YES";
        }else {
            cout<<"NO";
        }
    }

---
+++ Tickets.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int k=a*4;
+    if(k<=1000)
+    {
+        cout<<"YES";
+        
+    }else {
+        cout<<"NO";
+    }
+}
