6e63f319366944eaecd024ffbecfe81d24414fe0
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-03-22 19:09:29 +0530

    #include<iostream> using namespace std; int main() {     int a,c=0;     cin>>a;
        while(a!=0)
        {
           c=(c*10)+(a%10);
           a/=10;
           
        }
        cout<<c;
        
    }

---
+++ Reverse_of_a_Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,c=0;
+    cin>>a;
+    
+    while(a!=0)
+    {
+       c=(c*10)+(a%10);
+       a/=10;
+       
+    }
+    cout<<c;
+    
+}
